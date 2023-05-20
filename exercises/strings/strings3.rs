17c6bd9599f31d0d5d6649b0cc61c8b04d67ef3c
Oleg <voidshah@gmail.com>
2023-05-20 03:56:38 +0300

    strings

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,27 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // 1.
+    // input.to_string() + " world!"
+    // 2.
+    // let mut i = input.to_owned();
+    // i.push_str(" world!");
+    // i
+    // 3.
+    format!("{} {}", input.to_string(), "world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
